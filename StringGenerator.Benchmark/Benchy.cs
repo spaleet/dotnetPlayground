3b20f44f9e25a4ca74f7bf2424d52d09a9d519e7
Ali M <alimousavi-01@outlook.com>
2022-03-29 21:12:46 +0430

    Bug Fixes

--- StringGenerator.Benchmark/Benchy.cs
+++ StringGenerator.Benchmark/Benchy.cs
@@ -5,5 +5,15 @@ namespace StringGenerator.Benchmark;
 [MemoryDiagnoser]
 public class Benchy
 {
+    [Benchmark]
+    public void Old()
+    {
+        Generator.NewUserName();
+    }
 
+    [Benchmark]
+    public void New()
+    {
+        GeneratorT.NewUserName();
+    }
 }
