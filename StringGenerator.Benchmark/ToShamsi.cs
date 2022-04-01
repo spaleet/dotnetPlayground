a10c4811b29857d49afcb3204284b875c8bfbd79
Ali M <alimousavi-01@outlook.com>
2022-04-01 19:22:04 +0430

    Added PersianDate Tests

---
+++ StringGenerator.Benchmark/ToShamsi.cs
@@ -0,0 +1,36 @@
+﻿namespace StringGenerator.Benchmark
+{
+    public static class ToShamsi
+    {
+        /// <summary>
+        /// Get Shamsi Date From Miladi Year
+        /// </summary>
+        /// <param name="dateTime">Enter The Jalali DateTime</param>
+        /// <returns></returns>
+        public static string ToShamsiDate(this DateTime dateTime)
+        {
+            PersianDateShamsi persianDateShamsi = new PersianDateShamsi();
+            return persianDateShamsi.GetShamsiYearToString(dateTime) + "/" + persianDateShamsi.GetShamsiMonthString(dateTime) + "/" + persianDateShamsi.GetShamsiDayString(dateTime);
+        }
+        /// <summary>
+        /// Get Short Shamsi Date From Miladi Year
+        /// </summary>
+        /// <param name="dateTime">Enter The Jalali DateTime</param>
+        /// <returns></returns>
+        public static string ToShortShamsiDate(this DateTime dateTime)
+        {
+            PersianDateShamsi persianDateShamsi = new PersianDateShamsi();
+            return persianDateShamsi.GetShortShamsiYear(dateTime) + "/" + persianDateShamsi.GetShamsiMonthString(dateTime) + "/" + persianDateShamsi.GetShamsiDayString(dateTime);
+        }
+        /// <summary>
+        /// Get Long Shamsi Date From Miladi Year
+        /// </summary>
+        /// <param name="dateTime">Enter The Jalali DateTime</param>
+        /// <returns></returns>
+        public static string ToLongShamsiDate(this DateTime dateTime)
+        {
+            PersianDateShamsi persianDateShamsi = new PersianDateShamsi();
+            return persianDateShamsi.GetShamsiDayName(dateTime) + " " + persianDateShamsi.GetShamsiDay(dateTime) + " " + persianDateShamsi.GetShamsiMonthName(dateTime) + " " + persianDateShamsi.GetShamsiYear(dateTime);
+        }
+    }
+}
