6c8db04209ef8bc6cf0a12be2a0561a79a1314b5
Ali M <alimousavi-01@outlook.com>
2022-06-30 20:48:33 +0430

    Created SayHelloEvent

---
+++ Playground.Api/Events/SayHelloEvent.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+
+namespace Playground.Api.Events;
+
+public class SayHelloEvent : INotification
+{
+    public string Message { get; set; }
+}
+
+public class SayHelloEventHandle : INotificationHandler<SayHelloEvent>
+{
+    private readonly ILogger<SayHelloEvent> _logger;
+
+    public SayHelloEventHandle(ILogger<SayHelloEvent> logger)
+    {
+        _logger = logger;
+    }
+    
+    public Task Handle(SayHelloEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Event fired: {DomainEvent} Message: {msg}",
+                               notification.GetType().Name,
+                               notification.Message);
+
+        return Task.CompletedTask;
+    }
+}
+
