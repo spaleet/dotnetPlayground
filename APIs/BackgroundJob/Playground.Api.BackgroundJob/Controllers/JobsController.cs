b16e85fad11b00c4f1c6bd2fd54c85b5bb43c79a
Ali M <alimousavi-01@outlook.com>
2022-07-02 11:47:36 +0430

    Delayed

--- APIs/BackgroundJob/Playground.Api.BackgroundJob/Controllers/JobsController.cs
+++ APIs/BackgroundJob/Playground.Api.BackgroundJob/Controllers/JobsController.cs
@@ -7,25 +7,57 @@ namespace Playground.Api.BackgroundJob.Controllers;
 [Route("api/v1/[controller]")]
 public class JobsController : ControllerBase
 {
+    #region ctor
+
     private readonly ILogger<JobsController> _logger;
     private readonly IBackgroundJobClient _backgroundJobClient;
 
     public JobsController(ILogger<JobsController> logger, IBackgroundJobClient backgroundJobClient)
     {
-        _logger = logger; 
+        _logger = logger;
         _backgroundJobClient = backgroundJobClient;
     }
 
+    #endregion
+
+    #region Fire-And-Forget
+
     [HttpPost("fire-and-forget")]
-    public IActionResult FireAndForget([FromRoute] string message)
+    public IActionResult FireAndForget([FromQuery] string message)
     {
         string jobId = _backgroundJobClient.Enqueue(() => StartSomeTask(message));
 
         return Ok($"Job Id {jobId} Completed !");
     }
 
+    #endregion
 
-    private Task StartSomeTask(string message)
+    #region Delayed
+
+    [HttpPost("delayed")]
+    public IActionResult Delayed([FromQuery] string message, [FromQuery] int seconds)
+    {
+        string jobId = _backgroundJobClient.Schedule(() => StartSomeTask(message),
+                                                     TimeSpan.FromSeconds(seconds));
+
+        return Ok($"Job Id {jobId} Will Be Executed in {seconds} Seconds !");
+    }
+
+    [HttpPost("delayed/date")]
+    public IActionResult DelayedDate([FromQuery] string message)
+    {
+        var date = new DateTimeOffset(DateTime.Now.AddDays(1));
+
+        string jobId = _backgroundJobClient.Schedule(() => StartSomeTask(message), date);
+
+        return Ok($"Job Id {jobId} Will Be Executed in {date.Date:mm/dd/yyyy} !");
+    }
+
+    #endregion
+
+
+    [NonAction]
+    public Task StartSomeTask(string message)
     {
         // Some Task
         _logger.LogInformation("Message : {message}", message);
