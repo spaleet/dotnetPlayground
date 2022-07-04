4bf12a18238c991123bbff11d5b63aed00079625
Ali M <alimousavi-01@outlook.com>
2022-07-04 15:14:05 +0430

    Created Media Model

---
+++ APIs/MediaManager/Playground.Api.MediaManager/Data/Models/Media.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Playground.Api.MediaManager.Data.Models;
+
+public class Media
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public byte[] Content { get; set; }
+
+    public string ContentType { get; set; }
+
+    public string FileName { get; set; }
+}
