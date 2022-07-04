6985e264e233865f761007786d3973bf03d0d7dc
Ali M <alimousavi-01@outlook.com>
2022-07-04 15:08:35 +0430

    Created DbSettings

---
+++ APIs/MediaManager/Playground.Api.MediaManager/Data/DbSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Playground.Api.MediaManager.Data;
+
+public class DbSettings
+{
+    public string DbName { get; set; }
+
+    public string Host { get; set; }
+
+    public string Port { get; set; }
+
+    public string ConnectionString => $@"mongodb://{Host}:{Port}";
+}
