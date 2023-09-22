7c046eea61f3bc717fff2cb011bb6a3ebb726b48
Ali M <ali.mo.dev@gmail.com>
2023-09-22 21:10:14 +0330

    impelemented change image background

---
+++ APIs/FileStream/Playground.Api.FileStream/Controllers/PngToJpgController.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Playground.Api.FileStream.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PngToJpgController : ControllerBase
+{
+    private readonly IWebHostEnvironment _environment;
+
+    public PngToJpgController(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UploadImg(IFormFile file)
+    {
+        string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+        string path = Path.Combine(_environment.WebRootPath, $"upload/{fileName}");
+
+        using (var memoryStream = new MemoryStream())
+        {
+            await file.CopyToAsync(memoryStream);
+            using (var img = Image.FromStream(memoryStream))
+            {
+                Bitmap bmp = new Bitmap(img.Width, img.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.Clear(Color.White);
+                    g.DrawImage(img, new Rectangle(new Point(), img.Size), new Rectangle(new Point(), img.Size), GraphicsUnit.Pixel);
+                }
+                bmp.Save(path, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            }
+        }
+
+        return Ok();
+    }
+
+}
--- Console/Playground.Console.AddBackgroundToPng/Program.cs
+++ Console/Playground.Console.AddBackgroundToPng/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+
+Image imgFile = Image.FromFile(MergePath(@"\Images\cat.png"));
+imgFile.Save(MergePath(@"\Images\cat2.png"), System.Drawing.Imaging.ImageFormat.Jpeg);
+
+Console.WriteLine("Hello world!");
+
+
+static string MergePath(string path)
+{
+    string ret = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), path);
+
+    return ret;
+}
