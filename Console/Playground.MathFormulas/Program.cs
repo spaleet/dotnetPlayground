006f64e0a41b8c7a20a4d0804947ab45a6fbb3a2
Ali M <ali.mo.dev@gmail.com>
2024-04-14 00:26:47 +0330

    implemented Basic Variance calculator

--- Console/Playground.MathFormulas/Program.cs
+++ Console/Playground.MathFormulas/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Playground.MathFormulas.Variance_Calculator;
+
+var c = new VarianceCalc();
+
+var s = c.Calculate(new List<double>
+{
+    1, 3, 5, 7, 9
+});
+
+Console.WriteLine(s);
+Console.ReadLine();
---
+++ Console/Playground.MathFormulas/Variance Calculator/VarianceCalc.cs
@@ -0,0 +1,21 @@
+﻿namespace Playground.MathFormulas.Variance_Calculator;
+
+public class VarianceCalc
+{
+    public double Calculate(List<double> data)
+    {
+        var average = data.Average();
+
+        double numbersMinsAvgPow2 = 0;
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            var x = Math.Pow(data[i] - average, 2);
+            numbersMinsAvgPow2 = numbersMinsAvgPow2 + x;
+        }
+
+        var n = (data.Count - 1);
+
+        return (numbersMinsAvgPow2 / n);
+    }
+}
